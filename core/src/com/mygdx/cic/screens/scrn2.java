a8eb6679c77aa4eef9c44b734eb3071c0086e0f2
Arch-Frost <105542594+Arch-Frost@users.noreply.github.com>
2022-07-02 22:04:14 +0500

    Run glossy ui skin

--- core/src/com/mygdx/cic/screens/scrn2.java
+++ core/src/com/mygdx/cic/screens/scrn2.java
@@ -5,22 +5,53 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g3d.particles.values.UnweightedMeshSpawnShapeValue;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+import java.text.CollationElementIterator;
+
 public class scrn2 extends ScreenAdapter {
-     private Stage stage;
-     private Viewport viewport;
-     private AssetManager assetManager;
-     private Skin skin;
+
+    private Stage stage;
+
+    private Viewport viewport;
+    private AssetManager assetManager;
+    private Skin skin;
+    private Label outputLabel;
+
     @Override
     public void show(){
-        viewport=new ExtendViewport(1200,720);
-        stage=new Stage(viewport);
-        skin=new Skin(Gdx.files.internal("uiskin.json"));
+        viewport = new ScreenViewport();
+        stage = new Stage(viewport);
+        Gdx.input.setInputProcessor(stage);
+
+        int row_height = Gdx.graphics.getWidth() / 12;
+        int col_width = Gdx.graphics.getWidth() / 12;
+
+        Skin mySkin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+
+        Button button2 = new TextButton("Text Button",mySkin,"small");
+        button2.setSize(col_width*4,row_height);
+        button2.setPosition(col_width*7,Gdx.graphics.getHeight()-row_height*3);
+        button2.addListener(new InputListener(){
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                outputLabel.setText("Press a Button");
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                outputLabel.setText("Pressed Text Button");
+                return true;
+            }
+        });
+        stage.addActor(button2);
 
     }
     private TextButton addbutton(String name){
@@ -32,7 +63,7 @@ public class scrn2 extends ScreenAdapter {
 
     @Override
     public void render(float delta){
-        Gdx.gl.glClearColor(1f,1f,15f,1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
         stage.draw();
